59ba0ed979ad04895ce98f91ae676b3662113127
James Gruss, Jr <jgrussjr@gmail.com>
2015-12-30 22:22:42 -0500

    added new themes

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_site');
+define('DB_NAME', 'wp_wpsite');
 
 /** MySQL database username */
 define('DB_USER', 'wpadmin');
